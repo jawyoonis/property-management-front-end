d3f20299049af9fd9c2b5e99184677e8df014d87
Jamal Aw yonis <jamalawyonis@Jamals-MacBook-Pro.local>
2023-11-19 15:03:12 -0800

    created all the necessary components

--- property-management/src/app/app.module.ts
+++ property-management/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PropertiesComponent } from './src/app/component/properties/properties.component';
+import { OwnersComponent } from './src/app/component/owners/owners.component';
+import { LeasesComponent } from './src/app/component/leases/leases.component';
+import { TenantsComponent } from './src/app/component/tenants/tenants.component';
+import { PaymentsComponent } from './src/app/component/payments/payments.component';
+import { HeadersComponent } from './src/app/component/headers/headers.component';
+import { NavbarComponent } from './src/app/component/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertiesComponent,
+    OwnersComponent,
+    LeasesComponent,
+    TenantsComponent,
+    PaymentsComponent,
+    HeadersComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
